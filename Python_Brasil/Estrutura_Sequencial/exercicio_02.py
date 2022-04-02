4f7facd3dfbd7a47c1f77595018cd6fb0ee37bfd
Mateus Ceni <mateus.ceni@gmail.com>
2022-04-02 13:35:28 -0300

    exercicios_pratica 02

---
+++ Python_Brasil/Estrutura_Sequencial/exercicio_02.py
@@ -0,0 +1,10 @@
+'''
+EXERCICIO 02 - 02/04/22 - PYTHON BRASIL
+ESTRUTURA SEQUENCIAL
+
+2. Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+'''
+
+num = input('Digite um numero: ')
+
+print(f' O numero escolhido foi: {num}')
