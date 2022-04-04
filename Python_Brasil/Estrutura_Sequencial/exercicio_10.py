feb81a88d900f8a622501b6fa95efff1d97c041d
Mateus Ceni <mateus.ceni@gmail.com>
2022-04-03 22:04:06 -0300

    exercicios_pratico_Python_Brasil

--- Python_Brasil/Estrutura_Sequencial/exercicio_10.py
+++ Python_Brasil/Estrutura_Sequencial/exercicio_10.py
@@ -2,5 +2,10 @@
 EXERCICIO 10 - 03/04/22 - PYTHON BRASIL
 ESTRUTURA SEQUENCIAL
 
+10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 
 '''
+
+temperatura = int(input('Digite a temperatura em Celsius (de 0 até 100) a ser convertida:'))
+
+print(f'A temperatura é {temperatura * 1.8 + 32 } °F')
